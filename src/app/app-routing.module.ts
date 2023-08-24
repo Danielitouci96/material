5fffcdee7a29a659dbbb1ce8e0d1569b38b0f65f
danieluco <dpacheco@softel.cu>
2023-08-23 22:14:18 -0400

    agregado keycloak autentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './utility/app.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'dashboard', loadChildren: ()=> import('./components/dashboard/dashboard.module')
-  .then(x => x.DashboardModule) },
+  .then(x => x.DashboardModule), canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,11 @@ import { SharedModule } from './components/shared/shared.module';
 import { LoginComponent } from './components/login/login.component';
 import { MatChipsModule } from '@angular/material/chips';
 import { RegisterComponent } from './components/register/register.component';
+
+//keycloak imports
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from 'src/app/utility/app.init';
+
 
 
 @NgModule({
@@ -28,11 +33,19 @@ import { RegisterComponent } from './components/register/register.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    MatChipsModule
+    MatChipsModule,
+    KeycloakAngularModule
 
 
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
